5b4be2bd6aad5f4e73759af26504547d45f2a437
TheHackerOne <yash2200@gmail.com>
2020-03-25 23:47:18 +0530

    disjoint-set union

---
+++ disjoint-set/path_compression.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+// Disjoint set
+
+int parent[100];
+
+// function to find parent node of a node
+// path compression implementation
+int findParent(int a)
+{
+    if(parent[a]<0) return a;
+    return parent[a]=findParent(parent[a]);
+}
+
+// function to do union of two sets( connect two sets )
+void union1(int a, int b)
+{
+    parent[a] = parent[a] + parent[b];
+    // cout<<"Parent -> "<<parent[a]<<"\n";
+    parent[b] = a;
+}
+
+int main()
+{
+    int n, m, a, b;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+        parent[i] = -1;
+    for (int i = 0; i < m; i++)
+    {
+        cin >> a >> b;
+        a = findParent(a);
+        b = findParent(b);
+        if (a != b)
+            union1(a, b);
+    }
+    int ans = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        if (parent[i] < 0)
+            ans *= abs(parent[i]);
+    }
+    cout << ans << "\n";
+
+    return 0;
+}
---
+++ disjoint-set/teacher's_dilemma.cpp
@@ -0,0 +1,49 @@
+// https: //www.hackerearth.com/practice/data-structures/disjoint-data-strutures/basics-of-disjoint-data-structures/practice-problems/algorithm/teachers-dilemma-3/
+
+#include <iostream>
+using namespace std;
+
+// Disjoint set
+
+int parent[100];
+
+// function to find parent node of a node
+int findParent(int a)
+{
+    while (parent[a] > 0)
+        a = parent[a];
+    return a;
+}
+
+// function to do union of two sets( connect two sets )
+void union1(int a, int b)
+{
+    parent[a] = parent[a] + parent[b];
+    // cout<<"Parent -> "<<parent[a]<<"\n";
+    parent[b] = a;
+}
+
+int main()
+{
+    int n, m, a, b;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+        parent[i] = -1;
+    for (int i = 0; i < m; i++)
+    {
+        cin >> a >> b;
+        a = findParent(a);
+        b = findParent(b);
+        if (a != b)
+            union1(a, b);
+    }
+    int ans = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        if (parent[i] < 0)
+            ans *= abs(parent[i]);
+    }
+    cout << ans << "\n";
+
+    return 0;
+}
